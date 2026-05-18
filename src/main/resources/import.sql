edf097a6397717056775fa8d8a52f76e00dc8a42
Christiandg42 <christiandg42@educastur.es>
2026-05-18 15:00:28 +0200

    Add files via upload
    Añadir import sql con usuarios iniciales

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (username, password, email, fullname, role)
+VALUES
+    ('admin', '$2a$10$YTN9iDnU0Hnv/GrZOW13leDz9aaJq/Q4rzFD5VCxf3LM6r6PtaE5y', 'admin@email.com', 'Administrador', 'ADMIN'),
+    ('gestor', '$2a$10$YTN9iDnU0Hnv/GrZOW13leDz9aaJq/Q4rzFD5VCxf3LM6r6PtaE5y', 'gestor@email.com', 'Gestor', 'GESTOR'),
+    ('user', '$2a$10$YTN9iDnU0Hnv/GrZOW13leDz9aaJq/Q4rzFD5VCxf3LM6r6PtaE5y', 'user@email.com', 'Usuario', 'USER');
